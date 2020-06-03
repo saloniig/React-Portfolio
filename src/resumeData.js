7d2cca362a40b574e72d7363a2013a47a658e3f8
saloniig <saloniggoyal@gmail.com>
2020-06-03 14:53:01 +0530

    Portfolio working properly

---
+++ src/resumeData.js
@@ -0,0 +1,95 @@
+let resumeData={
+    "imagebaseurl":"http://localhost:3000/",
+    "name":"Saloni",
+    "role":"Currently Studying",
+    "linkedinId":"https://www.linkedin.com/in/saloni-goyal-9a5b1b193/",
+    "skypeid":"live:.cid.59b8762b9a3e9f9c",
+    "roleDescription":"I like triffle in frontend and web development and like to learn about new technologies, write technical articles or simply listem music in my free time.",
+    "socialLinks":[
+        {
+            "name":"linkedin",
+            "url":"https://www.linkedin.com/in/saloni-goyal-9a5b1b193",
+            "className":"fa fa-linkedin"
+          },
+          {
+            "name":"github",
+            "url":"http://github.com/saloniig",
+            "className":"fa fa-github"
+          },
+          {
+            "name":"skype",
+            "url":"http://skype.com",
+            "className":"fa fa-skype"
+          }
+        ],
+    "aboutme":"Currently,I am pursuing my B.Tech in Computer Science.",
+    "address":"India",
+    "website":"https://hackace2.wordpress.com/",
+    "education":[
+        {
+            "UniversityName":"Guru Nanak Dev Engineering College",
+            "specialization":"Ludhiana",
+            "MonthOfPassing":"Jan",
+            "YearOfPassing":"2018",
+            "Achievements":"Part of ISTE,Great Developers Team and CSI"
+      
+        },
+
+    ],
+    
+    "skillsDescription":"Skills:",
+  "skills":[
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"Reactjs"
+    },
+    {
+      "skillname":"Python"
+    },
+    {
+      "skillname":"Java"
+    },
+    {
+      "skillname":"Javascript"
+    },
+    {
+      "skillname":"CSS"
+    }
+   
+  ],
+  "portfolio":[
+    {
+      "name":"project1",
+      "description":"androidmobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    },
+    {
+      "name":"project2",
+      "description":"Online study system",
+      "imgurl":"images/portfolio/online.jpeg"
+    },
+    {
+      "name":"project3",
+      "description":"Chatbox",
+      "imgurl":"images/portfolio/chat.png"
+    },
+    
+    {
+      "name":"project5",
+      "description":"Event System",
+      "imgurl":"images/portfolio/events.jpeg"
+    }
+  ],
+  "testimonials":[
+    {
+      "description":"Mmmm,Whatcha Sayyy?",
+      "name":"Many people feel that there is a limited amount of abundance,wealth,or chances to succeed in life.Furthermore,there is a solid belief that if one person succeeds,another must fail. "
+    },
+
+  ]
+}
+
+
+export default resumeData;
